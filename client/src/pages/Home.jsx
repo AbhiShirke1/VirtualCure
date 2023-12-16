5fa99724658630657fa7c84f5be3bb96eae1ec76
AbhiShirke1 <abhishekshirke263@gmail.com>
2023-12-16 16:20:50 +0530

    project continuing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,11 @@ import heroImg01 from '../assets/images/hero-img01.png';
 import heroImg02 from '../assets/images/hero-img02.png';
 import heroImg03 from '../assets/images/hero-img03.png';
 import icon01 from '../assets/images/icon01.png';
+import icon02 from '../assets/images/icon02.png';
+import icon03 from '../assets/images/icon03.png';
 import {Link} from 'react-router-dom';
+import {BsArrowRight} from 'react-icons/bs'
+import About from '../components/About/About';
 
 const Home = () => {
   return (
@@ -65,7 +69,7 @@ const Home = () => {
 
     <section>
       <div className="container">
-        <div className='lg:w-[470px] max-auto'>
+        <div className=' max-auto'>
           <h2 className='heading text-center'>Providing the best medical services</h2>
           <p className='text_para text-center'>Best medical facilities for everyone. Our healthcare system offers unmatched care and experts</p>
         </div>
@@ -84,8 +88,43 @@ const Home = () => {
 
               <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>Best medical facilities for everyone. Our healthcare system offers unmatched care and experts</p>
 
-              <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181AE] mt-[30xp] mx-auto'>
+              <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex item-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white mt-[20%] w-6 h-5'/>
+              </Link>
+            </div>
+          </div>
+          <div className='py-[30px] px-5'>
+            <div className='flex item-center justify-center'>
+              <img src={icon02} alt="icon2" />
+            </div>
 
+            <div className='mt-[30px]'>
+              <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+                Find a Location
+              </h2>
+
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>Best medical facilities for everyone. Our healthcare system offers unmatched care and experts</p>
+
+              <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex item-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white mt-[20%] w-6 h-5'/>
+              </Link>
+            </div>
+          </div>
+
+          <div className='py-[30px] px-5'>
+            <div className='flex item-center justify-center'>
+              <img src={icon03} alt="icon3" />
+            </div>
+
+            <div className='mt-[30px]'>
+              <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+                Book Appointment
+              </h2>
+
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>Best medical facilities for everyone. Our healthcare system offers unmatched care and experts</p>
+
+              <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex item-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white mt-[20%] w-6 h-5'/>
               </Link>
             </div>
           </div>
@@ -93,6 +132,20 @@ const Home = () => {
         </div>
       </div>
     </section>
+
+    <About />
+
+    {/* services start */}
+    <section>
+      <div className="container">
+        <div className='max-auto'>
+          <h2 className='heading text-center'>Our medical services</h2>
+          <p className='text_para text-center'>Best care in the country. Unmatched care, facilities and experts in the sector.</p>
+        </div>
+      </div>
+    </section>
+    {/* services end */}
+
     </>
   )
 }
